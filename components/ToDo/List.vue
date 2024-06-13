12ec8faf56ce7da8acee52b771cb9ecec610cc28
Alpin Tri <alpintripranjadata@gmail.com>
2024-06-13 18:36:09 +0700

    follow-up interview session

--- components/ToDo/List.vue
+++ components/ToDo/List.vue
@@ -1,20 +1,24 @@
 <template>
-  <div class="fluid">
-    <label>Task</label>
-    <InputText v-model="taskDescription"></InputText>
-    <div class="fluid grid">
+  <div class="flex gap-1 md-flex-col">
+    <div class="flex flex-col">
+      <label>Task</label>
+      <InputText v-model="taskDescription"></InputText>
+    </div>
+    <div class="flex flex-col">
       <label> Duration </label>
-      <InputText v-model="durationTask"></InputText>
-      <Dropdown
-        v-model="selectedDescDuration"
-        :options="descriptionDuration"
-        optionLabel="name"
-      ></Dropdown>
-      <Button @click="createTask()" label="Add New Task"></Button>
+      <div class="flex gap-1">
+        <InputText v-model="durationTask"></InputText>
+        <Dropdown
+          v-model="selectedDescDuration"
+          :options="descriptionDuration"
+          optionLabel="name"
+        ></Dropdown>
+        <Button @click="createTask()" label="Add New Task"></Button>
+      </div>
     </div>
   </div>
-  <div class="fluid">
-    <Checkbox v-model="active">
+  <div class="flex mt-1 gap-0">
+    <Checkbox v-model="active" :value="true">
     </Checkbox>
     <label>
       Active
@@ -42,15 +46,15 @@
       <Column field="deletedAt" header="Deleted At"></Column>
       <Column header="Action">
         <template #body="slotProps">
-          <div v-if="!editable">
+          <div v-if="!editable" class="flex gap-0 md-flex-col">
             <Button label="Edit" @click="editText(slotProps.data.id)"></Button>
             <Button
               label="Soft Delete"
-              @click="deleteTask(slotProps.data.id)"
+              @click="deleteTask(slotProps.data.id, 'soft')"
             ></Button>
             <Button
               label="Hard Delete"
-              @click="deleteTask(slotProps.data.id)"
+              @click="deleteTask(slotProps.data.id, 'hard')"
             ></Button>
           </div>
           <div v-else>
@@ -65,6 +69,7 @@
 </template>
 
 <script setup>
+const active = ref(false)
 const taskList = ref();
 const taskDescription = ref();
 const durationTask = ref();
@@ -83,38 +88,114 @@ const selectedDescDuration = ref();
 onMounted(() => {
   loadData();
 });
-async function loadData() {
+async function loadData(state) {
   try {
-    const response = await $fetch(`/api/todos`);
-    taskList.value = response;
+    if (state === true) {
+      const response = await $fetch(`/api/todos?active=${true}`);
+      taskList.value = response;
+    } else {
+      const response = await $fetch(`/api/todos`);
+      taskList.value = response;
+    }
   } catch (error) {
     console.error("Cannot Access Server");
   }
 }
 async function createTask() {
   // TODO : Implement Create Task to Backend
+  const data = {
+    task: taskDescription.value,
+    duration: durationTask.value + selectedDescDuration.value.code,
+  }
+
+  try {
+    const res = await $fetch('/api/todos', {
+      body: data,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    loadData()
+    taskDescription.value = ''
+    durationTask.value = ''
+    selectedDescDuration.value = ''
+  } catch (error) {
+    console.log(error)
+  }
 }
 async function editText() {
   editable.value = true;
 }
 async function cancelEdit() {
   editable.value = false;
+  loadData()
 }
 async function updateTask(data) {
   // TODO : Implement Update Task to Backend
+  try {
+    const res = await $fetch(`/api/todos/${data.id}`, {
+      method: 'PATCH',
+      body: {
+        task: data.task,
+        duration: data.duration
+      }
+    })
+
+    // loadData()
+    // console.log(res)
+    editable.value = false
+    loadData()
+  } catch (error) {
+    console.log(error)
+  }
 }
-async function deleteTask(id) {
+async function deleteTask(id, typeDelete) {
   // TODO : Implement Condition Hard Delete
   try {
-    const response = await $fetch(`/api/todos/${id}`, {
+    // const url = typeDelete === 'hard' ? 
+    const response = await $fetch(`/api/todos/${id}?`, {
       method: "DELETE",
-      body: [],
     });
     return response;
   } catch (error) {
     console.error(error);
   }
 }
+
+watch(
+  active,
+  () => {
+    if (active.value[0]) {
+      loadData(true)
+    } else {
+      loadData()
+    }
+  }
+)
 </script>
 
-<style></style>
+<style scoped>
+.flex{
+  display: flex;
+}
+.flex-col{
+  flex-direction: column;
+}
+.gap-1{
+  gap: 1rem;
+}
+.mt-1{
+  margin-top: 1rem;
+}
+.gap-0{
+  gap: .5rem;
+}
+
+@media screen and (max-width: 900px){
+  .md-flex-col{
+    flex-direction: column;
+  }
+}
+</style>
